e1b318c1e2b911384d6eb36012799f075f6626e8
ciscoff <ciscoff@ya.ru>
2020-07-08 07:38:37 +0300

    + files

---
+++ app/src/main/java/s/yarlykov/izisandbox/ActivityGraph2.kt
@@ -0,0 +1,25 @@
+package s.yarlykov.izisandbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.Explode
+import android.transition.Fade
+import android.view.Window
+
+class ActivityGraph2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        with(window) {
+            requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+            requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+
+            enterTransition = Fade(Fade.IN).apply {
+                duration = 500
+            }
+            exitTransition = Explode()
+        }
+
+        setContentView(R.layout.activity_graph2)
+    }
+}
--- app/src/main/java/s/yarlykov/izisandbox/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/s/yarlykov/izisandbox/ui/slideshow/SlideshowFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import s.yarlykov.izisandbox.R
 
 class SlideshowFragment : Fragment() {
@@ -15,12 +15,12 @@ class SlideshowFragment : Fragment() {
     private lateinit var slideshowViewModel: SlideshowViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
+            ViewModelProvider(this).get(SlideshowViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
