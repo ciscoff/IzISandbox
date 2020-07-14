43ef9fb32092350b8baf2b6ca425d5ba0f05bf14
ciscoff <ciscoff@ya.ru>
2020-07-14 22:10:56 +0300

    Суппер !

--- app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_activities/ActivitySharedFrom.kt
+++ app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_activities/ActivitySharedFrom.kt
@@ -6,21 +6,68 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import s.yarlykov.izisandbox.R
+import s.yarlykov.izisandbox.transitions.shared_with_activities.ActivitySharedTo.Companion.KEY_BACKGROUND
 
 class ActivitySharedFrom : AppCompatActivity() {
 
+    private val child = listOf(
+        R.id.view_colored_blue,
+        R.id.view_colored_green,
+        R.id.view_colored_red,
+        R.id.view_colored_orange
+    )
+
+    private val colors = listOf(
+        R.drawable.shape_oval_blue,
+        R.drawable.shape_oval_green,
+        R.drawable.shape_oval_red,
+        R.drawable.shape_oval_orange
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_from)
 
-        findViewById<View>(R.id.view_red).apply {
-            isClickable = true
-            isFocusable = true
-            setOnClickListener(onClickListener)
+        for(i in child.indices) {
+
+            findViewById<View>(child[i]).apply {
+                isClickable = true
+                isFocusable = true
+                setOnClickListener(
+                    clickListenerFabric(
+                        R.string.shared_transition_colored,
+                        colors[i]
+                    )
+                )
+            }
         }
     }
 
+    private fun clickListenerFabric(sharedNameId: Int, backgroundColorId: Int): (View) -> Unit {
+
+        val activity = this@ActivitySharedFrom
+
+        return { sharedView ->
+
+            val intent = Intent(
+                activity,
+                ActivitySharedTo::class.java
+            ).apply {
+                putExtra(KEY_BACKGROUND, backgroundColorId)
+            }
+
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                activity,
+                sharedView,
+                getString(sharedNameId)
+            )
+            startActivity(intent, options.toBundle())
+        }
+    }
+
+    /**
+     * Базовый способ переключения активити
+     */
     private val onClickListener: (View) -> Unit = { sharedView ->
 
         val intent = Intent(this@ActivitySharedFrom, ActivitySharedTo::class.java)
@@ -28,7 +75,7 @@ class ActivitySharedFrom : AppCompatActivity() {
         val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
             this@ActivitySharedFrom,
             sharedView,
-            getString(R.string.shared_transition_red_ball)
+            getString(R.string.shared_transition_colored)
         )
         startActivity(intent, options.toBundle())
     }
--- app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_activities/ActivitySharedTo.kt
+++ app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_activities/ActivitySharedTo.kt
@@ -3,6 +3,7 @@ package s.yarlykov.izisandbox.transitions.shared_with_activities
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import s.yarlykov.izisandbox.R
 
 class ActivitySharedTo : AppCompatActivity() {
@@ -10,12 +11,24 @@ class ActivitySharedTo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_to)
 
-        findViewById<View>(R.id.view_red).apply {
+        findViewById<View>(R.id.view_colored).apply {
             isClickable = true
             isFocusable = true
+            background = ContextCompat.getDrawable(this@ActivitySharedTo, fetchBackground())
             setOnClickListener {
                 this@ActivitySharedTo.supportFinishAfterTransition()
             }
         }
+    }
+
+    private fun fetchBackground(): Int {
+        return intent?.getIntExtra(
+            KEY_BACKGROUND,
+            R.drawable.shape_oval_red
+        ) ?: R.drawable.shape_oval_red
+    }
+
+    companion object {
+        const val KEY_BACKGROUND = "KEY_BACKGROUND"
     }
 }
