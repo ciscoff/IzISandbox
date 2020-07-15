243241c62c21623cc5d0626e4a1fbc9843585037
ciscoff <ciscoff@ya.ru>
2020-07-15 20:00:01 +0300

    Протестрировал окно авторизации

---
+++ app/src/main/java/s/yarlykov/izisandbox/izilogin/IziLoginActivity.kt
@@ -0,0 +1,11 @@
+package s.yarlykov.izisandbox.izilogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IziLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_izi_login)
+    }
+}
