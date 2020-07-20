ce2837b8b9f80023f8ea392d907a124672e0fc81
ciscoff <ciscoff@ya.ru>
2020-07-20 11:43:45 +0300

    Fragment Shared View Animation OK

--- app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_fragments/ActivityWithFragments.kt
+++ app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_fragments/ActivityWithFragments.kt
@@ -1,15 +1,49 @@
-package s.yarlykov.izisandbox.transitions.shared_with_fragments
+package s.yarlykov.izisandbox.transitions.shared_with_fragments.one_container
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.flexbox.FlexboxLayout
 import s.yarlykov.izisandbox.R
+import s.yarlykov.izisandbox.transitions.shared_with_fragments.FragmentFrom
+import s.yarlykov.izisandbox.transitions.shared_with_fragments.FragmentLogo
 
 class ActivityWithFragments : AppCompatActivity() {
+
+    lateinit var buttonExample1 : Button
+    lateinit var buttonExample2 : Button
+    lateinit var flexBox : FlexboxLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_with_fragments)
 
+        findView()
+        initViews()
         initFragment(savedInstanceState)
+    }
+
+    private fun findView() {
+        flexBox = findViewById(R.id.flex_box)
+        buttonExample1 = findViewById(R.id.example_1)
+        buttonExample2 = findViewById(R.id.example_2)
+
+        setFlexboxChildLayoutParams(buttonExample1)
+        setFlexboxChildLayoutParams(buttonExample2)
+    }
+
+    private fun initViews() {
+        buttonExample1.setOnClickListener {
+            navigateTo(FragmentFrom::class.java, R.layout.fragment_shared_from_1)
+        }
+
+        buttonExample2.setOnClickListener {
+            navigateTo(FragmentFrom::class.java, R.layout.fragment_shared_from_2)
+        }
+
     }
 
     private fun initFragment(savedInstanceState: Bundle?) {
@@ -17,7 +51,39 @@ class ActivityWithFragments : AppCompatActivity() {
 
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.fragment_placeholder, FragmentFrom(), FragmentFrom::class.java.simpleName)
+            .replace(R.id.fragment_placeholder,
+                FragmentLogo(), FragmentLogo::class.java.simpleName)
             .commit()
+    }
+
+    private fun navigateTo(clazz : Class<*>, layoutId : Int) {
+
+        val fragment = clazz.getConstructor().newInstance() as Fragment
+
+        fragment.arguments?.putInt(FragmentFrom.KEY_LAYOUT_ID_FROM, layoutId)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder,
+                fragment, clazz.simpleName)
+            .commit()
+    }
+
+    /**
+     * Настройка LayoutParams для кнопок внутри макета Flexbox
+     *
+     * NOTE: Ширина устанавливается как процент от ширины родителя в диапазоне 0.0-1.0
+     */
+    private fun setFlexboxChildLayoutParams(
+        child: View,
+        childVisibility: Int = View.VISIBLE,
+        widthPercent: Float = 0.4F) {
+
+        child.apply {
+            visibility = childVisibility
+            layoutParams = (layoutParams as FlexboxLayout.LayoutParams).also {
+                it.flexBasisPercent = widthPercent
+            }
+        }
     }
 }
---
+++ app/src/main/java/s/yarlykov/izisandbox/transitions/shared_with_fragments/FragmentLogo.kt
@@ -0,0 +1,24 @@
+package s.yarlykov.izisandbox.transitions.shared_with_fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import s.yarlykov.izisandbox.R
+
+class FragmentTo : Fragment() {
+
+    companion object {
+        const val KEY_LAYOUT_ID_TO = "KEY_LAYOUT_ID_TO"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val layoutId = arguments?.getInt(KEY_LAYOUT_ID_TO) ?: R.layout.fragment_shared_to
+        return inflater.inflate(layoutId, container, false)
+    }
+}
