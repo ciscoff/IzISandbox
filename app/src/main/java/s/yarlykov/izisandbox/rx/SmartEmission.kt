67469bf9b4ba09e7924a90227a1a587081700521
ciscoff <ciscoff@ya.ru>
2020-07-27 19:52:53 +0300

    +

--- app/src/main/java/s/yarlykov/izisandbox/rx/SmartEmission.kt
+++ app/src/main/java/s/yarlykov/izisandbox/rx/SmartEmission.kt
@@ -1,2 +1,48 @@
 package s.yarlykov.izisandbox.rx
 
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
+import s.yarlykov.izisandbox.Utils.logIt
+import java.util.concurrent.TimeUnit
+
+
+enum class RunMode {
+    Stub,
+    Prod,
+    PreProd
+}
+
+fun main(args: Array<String>) {
+
+    val interval = Observable.interval(2, TimeUnit.SECONDS)
+    val source =
+        Observable.fromArray(
+            RunMode.PreProd,
+            RunMode.Prod,
+            RunMode.Prod,
+            RunMode.PreProd
+        )
+
+    val emission = Observable.zip(
+        interval,
+        source,
+        BiFunction<Long, RunMode, RunMode> { _, mode ->
+            println("next emission in ${System.currentTimeMillis()}, mode =${mode}")
+            mode
+        }
+    )
+
+    emission.scan (RunMode.Stub) { prev, next ->
+        println("prev=$prev, next=$next")
+        next
+    }.subscribe()
+
+    /**
+     * .subscribeOn(Schedulers.io())
+    .observeOn(AndroidSchedulers.mainThread())
+     */
+
+    Thread.sleep(20000)
+}
