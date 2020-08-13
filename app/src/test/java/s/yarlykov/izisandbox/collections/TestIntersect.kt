48029877e0a07b4d2717a3361ed0112286f2afc8
ciscoff <ciscoff@ya.ru>
2020-08-13 20:00:50 +0300

    ok

--- app/src/test/java/s/yarlykov/izisandbox/collections/TestIntersect.kt
+++ app/src/test/java/s/yarlykov/izisandbox/collections/TestIntersect.kt
@@ -1,2 +1,28 @@
 package s.yarlykov.izisandbox.collections
 
+import org.junit.Test
+
+class TestIntersect {
+
+     private val map = mapOf(
+        1 to setOf(10, 11, 15),
+        2 to setOf(11, 14, 10),
+        3 to setOf(17, 21, 11, 10),
+        4 to setOf(11, 16, 22, 10)
+    )
+
+
+    @Test
+    fun testIntersect() {
+
+        var result = mutableSetOf<Int>()
+
+            for((index, set) in map.values.withIndex()) {
+                if(index == 0) result.addAll(set)
+                result = result.intersect(set).toMutableSet()
+            }
+
+        assert(result.size == 2)
+        assert(result.size == 2 && result.contains(11) && result.contains(10))
+    }
+}
