5682988ceb1287bdd5d79cf4d57ef83e7734bde2
ciscoff <ciscoff@ya.ru>
2020-08-21 06:37:27 +0300

    animation OK

--- app/src/main/java/s/yarlykov/izisandbox/transitions/using_scenes/ScenesInsideActivity2.kt
+++ app/src/main/java/s/yarlykov/izisandbox/transitions/using_scenes/ScenesInsideActivity2.kt
@@ -2,13 +2,16 @@ package s.yarlykov.izisandbox.transitions.using_scenes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.transition.TransitionManager
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.google.android.material.card.MaterialCardView
 import s.yarlykov.izisandbox.R
 import s.yarlykov.izisandbox.extensions.dp
+import s.yarlykov.izisandbox.extensions.px
 
 class ScenesInsideActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,18 +22,28 @@ class ScenesInsideActivity2 : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        val sceneRoot = findViewById<FrameLayout>(R.id.scene_root)
+        val sceneRoot = findViewById<LinearLayout>(R.id.scene_root)
         val card = findViewById<MaterialCardView>(R.id.card)
-        animateCard(sceneRoot, card)
+
+        /**
+         * Анимацию запускаем с задержкой, чтобы весь макет успел полностью
+         * выполнить начальный layout после показа активити. Задержка в 10 мс
+         * маловата. 100 - как раз.
+         */
+        sceneRoot.postDelayed({
+            animateCard(sceneRoot, card)
+        }, 2000)
     }
 
+    /**
+     * Карточка анимированно "растет" от фиксированного положения своего top/left вправо и вниз.
+     */
     private fun animateCard(sceneRoot : ViewGroup, card : View) {
-
         TransitionManager.beginDelayedTransition(sceneRoot)
 
         val params = card.layoutParams as ViewGroup.LayoutParams
-        params.width = 300.dp
+        params.width = 200.px
+        params.height = 56.px
         card.layoutParams = params
-
     }
 }
