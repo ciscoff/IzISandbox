030781af95583c581e1da9b90efec8220f330e2b
ciscoff <ciscoff@ya.ru>
2020-09-23 12:22:55 +0300

    Анимация итемов RecyclerView c раскладкой Grid

---
+++ app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/layout_animation/GridRecyclerView.kt
@@ -0,0 +1,81 @@
+package s.yarlykov.izisandbox.recycler_and_swipes.layout_animation
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.GridLayoutAnimationController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import s.yarlykov.izisandbox.Utils.logIt
+
+/**
+ * Если View анимируется с помощью LayoutAnimation (а точнее с помощью layout animation controller),
+ * то ее LayoutParams получают дополнительные параметры, относящиеся к этой анимации. Эти
+ * дополнительные параметры упакованы в инстанс LayoutAnimationController.AnimationParameters,
+ * который содержит всего два Int-поля: count и index.
+ *   index - это индекс данного чайлда внутри родителя
+ *   count - это количество чайлдов у родителя.
+ * Благодаря этим значениям расчитывается start time анимации конкретного View.
+ * NOTE: index подразумевает сквозную индексацию детей, то есть как бы "линейно". Однако,
+ * если нужно оценить порядок детей с точки зрения "строка/столбец", то нужен иной подход.
+ * В этом случае используется GridLayoutAnimationController.AnimationParameters, у которого
+ * есть дополнительно columnsCount и rowsCount и об этом ниже.
+ *
+ * Так вот у ViewGroup есть метод attachLayoutAnimationParameters чтобы приаттачить параметры
+ * анимации к дочерним View. RecyclerView является ViewGroup, НО по умолчанию он считает, что его
+ * дети расположены линейно и использует LayoutAnimationController.AnimationParameters. Мы же хотим
+ * увидеть на экране Grid, поэтому переходим на GridLayoutAnimationController.AnimationParameters.
+ */
+
+class GridRecyclerView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    /**
+     * Задача следующая: на входе имеем index дочерней View и count - количество ВИДИМЫХ на экране
+     * дочерних элементов (похоже, что layout уже выполнен раз известно кол-во видимых элементов).
+     * Нужно превратить линейный index в двумерный column/row и записать результат в
+     * GridLayoutAnimationController.AnimationParameters.
+     *
+     * NOTE: Здесь расчет column/row выполняется через invertedIndex и получается криво, если
+     * количество видимых элементов не закрывают полностью экран. Нужно по другому сделать.
+     */
+    override fun attachLayoutAnimationParameters(
+        child: View,
+        params: ViewGroup.LayoutParams,
+        index: Int,
+        count: Int
+    ) {
+        val (adapter, layoutManager) = adapter to layoutManager
+
+        if (adapter != null && layoutManager is GridLayoutManager) {
+
+            val animationParams =
+                params.layoutAnimationParameters
+                        as? GridLayoutAnimationController.AnimationParameters
+                    ?: GridLayoutAnimationController.AnimationParameters()
+
+            params.layoutAnimationParameters = animationParams
+
+            val columns = layoutManager.spanCount
+
+            animationParams.count = count
+            animationParams.index = index
+            animationParams.columnsCount = columns
+            animationParams.rowsCount = count / columns
+
+            val invertedIndex = count - 1 - index
+
+            animationParams.column = columns - 1 - (invertedIndex % columns)
+            animationParams.row = animationParams.rowsCount - 1 - invertedIndex / columns
+
+            animationParams.apply {
+                logIt("count=$count, index=$index, invertedIndex=$invertedIndex, column=$column, row=$row")
+            }
+
+        } else {
+            super.attachLayoutAnimationParameters(child, params, index, count)
+        }
+    }
+}
--- app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/layout_animation/RecyclerViewLayoutAnimationActivity.kt
+++ app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/layout_animation/RecyclerViewLayoutAnimationActivity.kt
@@ -3,18 +3,28 @@ package s.yarlykov.izisandbox.recycler_and_swipes.layout_animation
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_recycler_view_layout_animation.*
 import s.yarlykov.izisandbox.R
 
 class RecyclerViewLayoutAnimationActivity : AppCompatActivity() {
+
+    lateinit var itemDecorator : ItemOffsetDecoration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view_layout_animation)
         setSupportActionBar(toolbar)
+
+        rvLinear.visibility = View.INVISIBLE
+        rvGrid.visibility = View.INVISIBLE
+
+        itemDecorator = ItemOffsetDecoration(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,16 +41,23 @@ class RecyclerViewLayoutAnimationActivity : AppCompatActivity() {
     }
 
     private fun animateLinear(): Boolean {
-
         val animationController =
             AnimationUtils.loadLayoutAnimation(this, R.anim.layout_animation_fall_down)
 
-        rvAnimated.apply {
+        rvLinear.apply {
+
+            // Нужно удалять, иначе при каждой следующей анимации суммируются отступы
+            // пердыдущего декоратора.
+            removeItemDecoration(itemDecorator)
+
+            rvGrid.visibility = View.INVISIBLE
+            visibility = View.VISIBLE
+
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@RecyclerViewLayoutAnimationActivity)
             layoutAnimation = animationController
             itemAnimator = DefaultItemAnimator()
-
+            addItemDecoration(itemDecorator)
             adapter = AdapterLinear()
             scheduleLayoutAnimation()
         }
@@ -49,7 +66,28 @@ class RecyclerViewLayoutAnimationActivity : AppCompatActivity() {
     }
 
     private fun animateGrid(): Boolean {
-        // TODO
+
+        val columns = 4
+        val rows = 16
+
+        val animationController =
+            AnimationUtils.loadLayoutAnimation(this, R.anim.grid_layout_animation_from_bottom)
+
+        rvGrid.apply {
+
+            rvLinear.visibility = View.INVISIBLE
+            visibility = View.VISIBLE
+
+            // Нужно удалять, иначе при каждой следующей анимации суммируются отступы
+            // пердыдущего декоратора.
+            removeItemDecoration(itemDecorator)
+
+            layoutManager = GridLayoutManager(context, columns)
+            layoutAnimation = animationController
+            adapter = AdapterGrid(columns * rows)
+            addItemDecoration(itemDecorator)
+            scheduleLayoutAnimation()
+        }
 
         return false
     }
