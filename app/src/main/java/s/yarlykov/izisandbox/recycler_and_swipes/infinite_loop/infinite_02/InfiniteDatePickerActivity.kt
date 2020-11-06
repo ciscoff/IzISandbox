4af601f2f72934753ecee85c637d816f11f40a3f
ciscoff <ciscoff@ya.ru>
2020-11-06 22:36:13 +0300

    Готовый вариант с автоматической центровкой по окончании прокрутки

--- app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/infinite_loop/infinite_02/InfiniteDatePickerActivity.kt
+++ app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/infinite_loop/infinite_02/InfiniteDatePickerActivity.kt
@@ -2,6 +2,7 @@ package s.yarlykov.izisandbox.recycler_and_swipes.infinite_loop.infinite_02
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearSnapHelper
 import kotlinx.android.synthetic.main.activity_infinite_date_picker.*
 import s.yarlykov.izisandbox.R
 
@@ -18,6 +19,23 @@ class InfiniteDatePickerActivity : AppCompatActivity() {
         infinitePicker.apply {
             layoutManager = LoopRecyclerManagerV02(model)
             adapter = AdapterDates(model)
+        }
+
+        /**
+         * Данный класс центрирует child'а RecyclerView, который ПОСЛЕ окончания пользовательской
+         * прокрутки ближе всего к центру. Остальные child'ы также смещаются.
+         *
+         * Примерный смысл его действий следующий: установить в RecyclerView свой обработчик
+         * OnScrollListener. По окончании пользовательской прокрутки, используя LayoutManager
+         * пройтись по child'ам, в поисках того, который ближе к центру (это называется snap view).
+         * Затем с помощью своего SmoothScroller'а и вызова LayoutManager.startSmoothScroll
+         * анимированно дотянуть контент таким образом, чтобы snap view встал четко по центру.
+         *
+         * NOTE: Этим классом пользуется ViewPager2, чтобы устанавливать контент очередной страницы
+         * внутри своего viewPort.
+         */
+        LinearSnapHelper().apply {
+            attachToRecyclerView(infinitePicker)
         }
     }
 
