82696e3b8bab24ef1402120a40e8f07360335f30
ciscoff <ciscoff@ya.ru>
2020-11-26 12:09:55 +0300

    + Рамка вокруг области выделения

--- app/src/main/java/s/yarlykov/izisandbox/matrix/avatar_maker/AvatarPreView.kt
+++ app/src/main/java/s/yarlykov/izisandbox/matrix/avatar_maker/AvatarPreView.kt
@@ -5,6 +5,8 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import s.yarlykov.izisandbox.R
+import s.yarlykov.izisandbox.dsl.extenstions.dp_f
+import s.yarlykov.izisandbox.utils.logIt
 import kotlin.math.min
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
@@ -14,6 +16,21 @@ class AvatarPreView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    /**
+     * Ширина линии рамки
+     */
+    private val borderWidth = dp_f(2f)
+
+    /**
+     * Изменился размер View. Потребуется пересчет всех компонентов.
+     */
+    private var sizeChanged = false
+
+    /**
+     * Раидиус закругления рамки
+     */
+    private val cornerRadius = dp_f(2f)
 
     /**
      * Исходная Bitmap и её размеры
@@ -39,9 +56,16 @@ class AvatarPreView @JvmOverloads constructor(
     private val rectClip: RectF by rectClipDelegate()
 
     /**
+     * @rectBorder прямоугольник для рамки
+     */
+    private val rectBorder: RectF by rectBorderDelegate()
+
+    /**
      * @pathClip определяется через @rectClip.
      */
     private val pathClip = Path()
+
+    private val pathBorder = Path()
 
     /**
      * Цветовые фильтры поярче/потемнее.
@@ -54,8 +78,20 @@ class AvatarPreView @JvmOverloads constructor(
      */
     private val paintBackground = Paint(Color.GRAY).apply { colorFilter = colorFilterDarken }
 
+    /**
+     * Paint (color Yellow)
+     */
+    private val paintStroke: Paint = Paint().apply {
+        color = Color.argb(0xff, 0xff, 0xff, 0x0)
+        style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = borderWidth
+        isAntiAlias = true
+    }
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+
         sourceImageBitmap =
             BitmapFactory.decodeResource(context.resources, R.drawable.nature)?.also {
                 rectSourceImage = Rect(0, 0, it.width, it.height)
@@ -64,22 +100,67 @@ class AvatarPreView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-        sourceImageBitmap?.let {
-            canvas.drawBitmap(it, rectSourceImage, rectDest, paintBackground)
-        }
+        // Background
+        drawLayer1(canvas)
 
         sourceImageBitmap?.let {
             pathClip.apply {
                 reset()
-                addRect(rectClip, Path.Direction.CW)
+                addRoundRect(rectClip, cornerRadius, cornerRadius, Path.Direction.CW)
             }.close()
 
             canvas.save()
             canvas.clipPath(pathClip)
-            canvas.drawBitmap(it, rectSourceImage, rectDest, null)
+            // Выделенная область
+            drawLayer2(canvas, it)
+            // Рамка вокруг выделенной области
+            drawLayer3(canvas)
             canvas.restore()
+
+            sizeChanged = false
         }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        sizeChanged = true
+    }
+
+    /**
+     * Background
+     */
+    private fun drawLayer1(canvas: Canvas) {
+        sourceImageBitmap?.let {
+            canvas.drawBitmap(it, rectSourceImage, rectDest, paintBackground)
+        }
+    }
+
+    /**
+     * Выделенная область
+     */
+    private fun drawLayer2(canvas: Canvas, bitmap: Bitmap) {
+        canvas.drawBitmap(bitmap, rectSourceImage, rectDest, null)
+    }
+
+    /**
+     * Рамка вокруг выделенной области
+     */
+    private fun drawLayer3(canvas: Canvas) {
+
+        pathBorder.apply {
+            reset()
+            moveTo(rectBorder.left, rectBorder.top + rectBorder.height() / 4f)
+            lineTo(rectBorder.left, rectBorder.top)
+            lineTo(rectBorder.left + rectBorder.width(), rectBorder.top)
+            lineTo(rectBorder.left + rectBorder.width(), rectBorder.top + rectBorder.height())
+            lineTo(rectBorder.left, rectBorder.top + rectBorder.height())
+            close()
+        }
+
+        paintStroke.pathEffect =
+            DashPathEffect(floatArrayOf(rectBorder.width() / 2f, rectBorder.width() / 2), 0f)
+        canvas.drawPath(pathBorder, paintStroke)
+
     }
 
     /**
@@ -97,15 +178,20 @@ class AvatarPreView @JvmOverloads constructor(
             var rect = Rect()
 
             override fun getValue(thisRef: Any?, property: KProperty<*>): Rect {
-                val ratio = height.toFloat() / rectSourceImage.height()
-                val scaledWidth = (rectSourceImage.width() * ratio).toInt()
 
-                return rect.apply {
-                    top = 0
-                    bottom = this@AvatarPreView.height
-                    left = (this@AvatarPreView.width - scaledWidth) / 2
-                    right = left + scaledWidth
+                if (rect.isEmpty || sizeChanged) {
+
+                    val ratio = height.toFloat() / rectSourceImage.height()
+                    val scaledWidth = (rectSourceImage.width() * ratio).toInt()
+
+                    rect.apply {
+                        top = 0
+                        bottom = this@AvatarPreView.height
+                        left = ((this@AvatarPreView.width - scaledWidth) / 2f).toInt()
+                        right = left + scaledWidth
+                    }
                 }
+                return rect
             }
 
             override fun setValue(thisRef: Any?, property: KProperty<*>, value: Rect) {}
@@ -120,14 +206,19 @@ class AvatarPreView @JvmOverloads constructor(
             var rect = Rect()
 
             override fun getValue(thisRef: Any?, property: KProperty<*>): Rect {
-                return rect.apply {
-                    top = 0
-                    bottom = this@AvatarPreView.height
-                    left =
-                        if (rectDest.left <= 0) 0 else rectDest.left
-                    right =
-                        if (rectDest.right >= this@AvatarPreView.width) this@AvatarPreView.width else rectDest.right
+
+                if (rect.isEmpty || sizeChanged) {
+                    rect.apply {
+                        top = 0
+                        bottom = this@AvatarPreView.height
+                        left =
+                            if (rectDest.left <= 0) 0 else rectDest.left
+                        right =
+                            if (rectDest.right >= this@AvatarPreView.width) this@AvatarPreView.width else rectDest.right
+                    }
                 }
+
+                return rect
             }
 
             override fun setValue(thisRef: Any?, property: KProperty<*>, value: Rect) {}
@@ -143,25 +234,54 @@ class AvatarPreView @JvmOverloads constructor(
 
             override fun getValue(thisRef: Any?, property: KProperty<*>): RectF {
 
-                val isVertical = rectVisible.height() >= rectVisible.width()
-                val frameDimen = min(rectVisible.height(), rectVisible.width())
+                if (rect.isEmpty || sizeChanged) {
 
-                return rect.apply {
-                    top = if (isVertical) {
-                        (rectVisible.height() - frameDimen) / 2f
-                    } else {
-                        rectVisible.top.toFloat()
+                    val isVertical = rectVisible.height() >= rectVisible.width()
+                    val frameDimen = min(rectVisible.height(), rectVisible.width())
+
+                    rect.apply {
+                        top = if (isVertical) {
+                            (rectVisible.height() - frameDimen) / 2f
+                        } else {
+                            rectVisible.top.toFloat()
+                        }
+
+                        bottom = top + frameDimen
+
+                        left = if (isVertical) {
+                            rectVisible.left.toFloat()
+                        } else {
+                            (rectVisible.width() - frameDimen) / 2f
+                        }
+                        right = left + frameDimen
                     }
-
-                    bottom = top + frameDimen
-
-                    left = if (isVertical) {
-                        rectVisible.left.toFloat()
-                    } else {
-                        (rectVisible.width() - frameDimen) / 2f
-                    }
-                    right = left + frameDimen
                 }
+                return rect
+            }
+
+            override fun setValue(thisRef: Any?, property: KProperty<*>, value: RectF) {}
+        }
+
+    /**
+     * Делегат зависит от rectVisible
+     */
+    private fun rectBorderDelegate(): ReadWriteProperty<Any?, RectF> =
+        object : ReadWriteProperty<Any?, RectF> {
+
+            var rect = RectF()
+
+            override fun getValue(thisRef: Any?, property: KProperty<*>): RectF {
+
+                if (rect.isEmpty || sizeChanged) {
+
+                    rect.apply {
+                        left = rectClip.left + borderWidth / 2f
+                        top = rectClip.top + borderWidth / 2f
+                        right = rectClip.right - borderWidth / 2f
+                        bottom = rectClip.bottom - borderWidth / 2f
+                    }
+                }
+                return rect
             }
 
             override fun setValue(thisRef: Any?, property: KProperty<*>, value: RectF) {}
