192b669235a6481e516cf373e55f8da202847ddd
ciscoff <ciscoff@ya.ru>
2020-12-10 12:31:13 +0300

    Не

--- app/src/main/java/s/yarlykov/izisandbox/matrix/surface/v02/RoadBook.kt
+++
@@ -1,5 +0,0 @@
-package s.yarlykov.izisandbox.matrix.surface.v02
-
-import android.graphics.PointF
-
-data class RoadBook(val redrawInterval: Long, val points: Array<PointF>)
--- app/src/main/java/s/yarlykov/izisandbox/matrix/surface/v02/RoadBookType.kt
+++
@@ -1,8 +0,0 @@
-package s.yarlykov.izisandbox.matrix.surface.v02
-
-enum class RoadBookType {
-    Slow,
-    Moderate,
-    Fast,
-    Instantly
-}
