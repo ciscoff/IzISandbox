6a291ff8080133695256dcd6231cc2f11b156b06
ciscoff <ciscoff@ya.ru>
2021-01-19 10:22:30 +0300

    2d scroll корова на льду

--- app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/time_line_2d/TimeLineLayoutManager.kt
+++ app/src/main/java/s/yarlykov/izisandbox/recycler_and_swipes/time_line_2d/TimeLineLayoutManager.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import s.yarlykov.izisandbox.R
+import s.yarlykov.izisandbox.utils.logIt
 import kotlin.math.max
+import kotlin.math.min
 
 class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager() {
 
@@ -23,7 +25,7 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
         context.resources.getDimensionPixelSize(R.dimen.column_width)
     }
 
-    private var scaleHeight = 1
+    private var scaleHeight = 2
 
     private val viewCache = SparseArray<View>()
 
@@ -48,65 +50,27 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
         val anchorView = getAnchorView()
         viewCache.clear()
 
-        //Помещаем вьюшки в кэш и...
+        // Помещаем вьюшки в cache и...
         for (i in 0 until childCount) {
             getChildAt(i)?.let { view ->
                 viewCache.put(getPosition(view), view)
             }
         }
 
-        //... и удалям из лэйаута
+        //... и удалям из layout'a
         for (i in 0 until viewCache.size()) {
             detachView(viewCache.valueAt(i))
         }
 
+        // Заполнить список элементами
         fillLeft(anchorView, recycler)
         fillRight(anchorView, recycler)
 
+        // Удалить элементы, который вышли из области видимости.
         for (i in 0 until viewCache.size()) {
             recycler.recycleView(viewCache.valueAt(i))
         }
-
-//        fillRow(recycler)
     }
-
-    /**
-     * Сделать layout элементов одной строки
-     */
-    private fun fillRow(recycler: RecyclerView.Recycler) {
-
-        var nextItem = 0
-        summaryWidth = 0
-
-        val widthSpec = View.MeasureSpec.makeMeasureSpec(spanSize, View.MeasureSpec.EXACTLY)
-        val heightSpec =
-            View.MeasureSpec.makeMeasureSpec(height - paddingTop, View.MeasureSpec.EXACTLY)
-
-        var viewLeft = paddingLeft
-        val viewTop = paddingTop
-
-        while (nextItem != itemCount && viewLeft < width) {
-
-            val child = recycler.getViewForPosition(nextItem)
-            addView(child)
-
-            measureChildWithoutInsets(child, widthSpec, heightSpec)
-
-            val decoratedMeasuredWidth = getDecoratedMeasuredWidth(child)
-            val decoratedMeasuredHeight = getDecoratedMeasuredHeight(child)
-
-            layoutDecorated(
-                child,
-                viewLeft,
-                viewTop,
-                viewLeft + decoratedMeasuredWidth,
-                viewTop + decoratedMeasuredHeight
-            )
-            viewLeft = getDecoratedRight(child)
-            nextItem++
-        }
-    }
-
 
     /**
      * Установить "чистый" размер view - без inset'ов, а именно:
@@ -122,7 +86,7 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
         resetMargins(child)
 
         val widthSpecUpdated = updateMeasureSpecs(widthSpec, spanSize)
-        val heightSpecUpdated = updateMeasureSpecs(heightSpec, height - paddingTop)
+        val heightSpecUpdated = updateMeasureSpecs(heightSpec, (height - paddingTop) * scaleHeight)
         child.measure(widthSpecUpdated, heightSpecUpdated)
     }
 
@@ -152,29 +116,84 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
     }
 
     override fun scrollHorizontallyBy(
-        dx: Int,
-        recycler: RecyclerView.Recycler?,
+        dX: Int,
+        recycler: RecyclerView.Recycler,
         state: RecyclerView.State?
     ): Int {
-        return super.scrollHorizontallyBy(dx, recycler, state)
+        val delta = scrollHorizontallyInternal(dX)
+
+        if (delta != 0) {
+            offsetChildrenHorizontal(-delta)
+            fill(recycler)
+        }
+
+        return delta
+    }
+
+    override fun scrollVerticallyBy(
+        dY: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State?
+    ): Int {
+
+        val delta =  when {
+            // Палец идет вверх. Контролируем появление View снизу.
+            (dY > 0) -> translateUp(dY, recycler)
+            // Палец идет вниз. Контролируем появление View сверху.
+            (dY < 0) -> translateDown(dY, recycler)
+            else -> {
+                0
+            }
+        }
+
+        if(delta != 0) offsetChildrenVertical(-delta)
+
+        logIt("Translate vert $delta")
+
+        return delta
+    }
+
+    private fun translateUp(dY: Int, recycler: RecyclerView.Recycler): Int {
+
+        return getChildAt(0)?.let { firstChild ->
+
+            val bottomView = getDecoratedBottom(firstChild)
+            if (bottomView == height) return 0
+
+            min(dY, bottomView - height)
+
+        } ?: 0
+    }
+
+    private fun translateDown(dY: Int, recycler: RecyclerView.Recycler): Int {
+
+        return getChildAt(0)?.let { firstChild ->
+
+            val topView = getDecoratedTop(firstChild)
+            if (topView == paddingTop) return 0
+            max(dY, topView - height)
+
+        } ?: 0
+
     }
 
 
     private fun fillLeft(anchorView: View?, recycler: RecyclerView.Recycler) {
 
-        val (anchorPos, anchorLeft) = if (anchorView != null) {
-            getPosition(anchorView) to getDecoratedLeft(anchorView)
-        } else 0 to paddingLeft
-
-        var fillLeft = true
-        var pos = anchorPos - 1
+        val (anchorPos, anchorLeft) =
+            if (anchorView != null) {
+                getPosition(anchorView) to getDecoratedLeft(anchorView)
+            } else 0 to paddingLeft
 
         var viewRight = anchorLeft
-        val viewHeight = height - paddingTop
+        val viewHeight = (height - paddingTop) * scaleHeight
         val viewTop = paddingTop
 
         val widthSpec = View.MeasureSpec.makeMeasureSpec(spanSize, View.MeasureSpec.EXACTLY)
         val heightSpec = View.MeasureSpec.makeMeasureSpec(viewHeight, View.MeasureSpec.EXACTLY)
+
+        var pos = anchorPos - 1
+        var fillLeft = viewRight > paddingLeft
 
         while (fillLeft && pos >= 0) {
             var child = viewCache.get(pos) //проверяем кэш
@@ -214,19 +233,17 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
 
         val (anchorPos, anchorRight) = if (anchorView != null) {
             getPosition(anchorView) to getDecoratedRight(anchorView)
-        } else 0 to 0
+        } else 0 to paddingLeft
 
-        var pos = anchorPos
-        var fillRight = true
-        val height = height - paddingTop
         var viewLeft = anchorRight
         val viewTop = paddingTop
-
-        val itemCount = itemCount
-        val viewHeight = height - paddingTop
+        val viewHeight = (height - paddingTop) * scaleHeight
 
         val widthSpec = View.MeasureSpec.makeMeasureSpec(spanSize, View.MeasureSpec.EXACTLY)
         val heightSpec = View.MeasureSpec.makeMeasureSpec(viewHeight, View.MeasureSpec.EXACTLY)
+
+        var pos = anchorPos
+        var fillRight = viewLeft < width
 
         while (fillRight && pos < itemCount) {
 
@@ -258,7 +275,10 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
     }
 
     /**
-     * Найти "опорную" view
+     * Найти "опорную" view. Поиск по наибольшей площади child'а. Если несколько child'ов
+     * имеют одинаковую максимальную площадь, то по умолчанию в hashMap'е останется последний
+     * добавленный. Однако я изменил условия добавления. Остается первый добавленный. Это помогло
+     * решить проблему с горизонтальным скролом слева направо.
      */
     private fun getAnchorView(): View? {
 
@@ -277,18 +297,70 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
                 val intersect = viewRect.intersect(mainRect)
                 if (intersect) {
                     val square = viewRect.width() * viewRect.height()
-                    viewsOnScreen[square] = child
+                    if (!viewsOnScreen.containsKey(square)) {
+                        viewsOnScreen[square] = child
+                    }
                 }
             }
         }
         if (viewsOnScreen.isEmpty()) {
             return null
         }
-        var maxSquare: Int? = null
+        var maxSquare = 0
         for (square in viewsOnScreen.keys) {
-            maxSquare = if (maxSquare == null) square else max(maxSquare, square)
+            maxSquare = max(maxSquare, square)
         }
         return viewsOnScreen[maxSquare]
+    }
+
+    private fun scrollHorizontallyInternal(dX: Int): Int {
+
+        if (childCount == 0) {
+            return 0
+        }
+
+//        val viewPortWidth = width - paddingLeft
+//        val rightView = getChildAt(childCount - 1)!!
+//        val leftView = getChildAt(0)!!
+//        val viewSpan = getDecoratedRight(rightView) - getDecoratedLeft(leftView)
+//        if (viewSpan <= viewPortWidth && getPosition(rightView) == (itemCount - 1)) {
+//            return 0
+//        }
+//        logIt("scrollHorizontallyInternal childCount=$childCount, viewPortWidth=$viewPortWidth")
+
+        var delta = 0
+
+        // Палец идет вправо. Контролируем появление элементов слева.
+        if (dX < 0) {
+
+            val firstVisibleView = getChildAt(0)!!
+            val firstVisibleViewAdapterPos = getPosition(firstVisibleView)
+
+            val d = getDecoratedLeft(firstVisibleView)
+            val d1 = paddingLeft
+
+            delta = if (firstVisibleViewAdapterPos > 0) {
+//                logIt("firstVisibleViewAdapterPos =$firstVisibleViewAdapterPos, dX=$dX")
+                dX
+            } else {
+//                logIt("firstVisibleViewAdapterPos =$firstVisibleViewAdapterPos, getDecoratedLeft=$d, paddingLeft=$d1, dX=$dX")
+                max(getDecoratedLeft(firstVisibleView) - paddingLeft, dX)
+            }
+        }
+        // Палец идет влево. Контролируем появление элементов справа.
+        else if (dX > 0) {
+
+            val lastView = getChildAt(childCount - 1)!!
+            val lastViewAdapterPos = getPosition(lastView)
+
+            delta =
+                if (lastViewAdapterPos < itemCount - 1) {
+                    dX
+                } else {
+                    min(getDecoratedRight(lastView) - width, dX)
+                }
+        }
+        return delta
     }
 
     override fun canScrollHorizontally(): Boolean = true
@@ -299,5 +371,42 @@ class TimeLineLayoutManager(val context: Context) : RecyclerView.LayoutManager()
             RecyclerView.LayoutParams.WRAP_CONTENT,
             RecyclerView.LayoutParams.WRAP_CONTENT
         )
+    }
+
+    /**
+     * Сделать layout элементов одной строки
+     */
+    private fun fillRow(recycler: RecyclerView.Recycler) {
+
+        var nextItem = 0
+        summaryWidth = 0
+
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(spanSize, View.MeasureSpec.EXACTLY)
+        val heightSpec =
+            View.MeasureSpec.makeMeasureSpec(height - paddingTop, View.MeasureSpec.EXACTLY)
+
+        var viewLeft = paddingLeft
+        val viewTop = paddingTop
+
+        while (nextItem != itemCount && viewLeft < width) {
+
+            val child = recycler.getViewForPosition(nextItem)
+            addView(child)
+
+            measureChildWithoutInsets(child, widthSpec, heightSpec)
+
+            val decoratedMeasuredWidth = getDecoratedMeasuredWidth(child)
+            val decoratedMeasuredHeight = getDecoratedMeasuredHeight(child)
+
+            layoutDecorated(
+                child,
+                viewLeft,
+                viewTop,
+                viewLeft + decoratedMeasuredWidth,
+                viewTop + decoratedMeasuredHeight
+            )
+            viewLeft = getDecoratedRight(child)
+            nextItem++
+        }
     }
 }
