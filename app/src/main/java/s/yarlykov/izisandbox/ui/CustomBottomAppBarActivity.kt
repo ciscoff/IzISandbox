d3800d2e36a00b78d3aa260b87f182a584de33f8
ciscoff <ciscoff@ya.ru>
2021-01-29 13:48:26 +0300

    +

--- app/src/main/java/s/yarlykov/izisandbox/ui/CustomBottomAppBarActivity.kt
+++ app/src/main/java/s/yarlykov/izisandbox/ui/CustomBottomAppBarActivity.kt
@@ -1,11 +1,20 @@
 package s.yarlykov.izisandbox.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ValueAnimator
+import android.animation.ValueAnimator.INFINITE
+import android.animation.ValueAnimator.REVERSE
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.text.TextPaint
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomappbar.BottomAppBar
 import kotlinx.android.synthetic.main.activity_custom_bottom_app_bar.*
 import s.yarlykov.izisandbox.R
+import s.yarlykov.izisandbox.utils.logIt
 import kotlin.properties.Delegates
 
 class CustomBottomAppBarActivity : AppCompatActivity(), View.OnClickListener {
@@ -31,6 +40,11 @@ class CustomBottomAppBarActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        animateTextColors()
+    }
+
     override fun onClick(v: View?) {
         if (isBarVisible) {
             bottomBar.performHide()
@@ -39,6 +53,46 @@ class CustomBottomAppBarActivity : AppCompatActivity(), View.OnClickListener {
             bottomBar.performShow()
         }
         isBarVisible = !isBarVisible
+    }
+
+    /**
+     * ValueAnimator будет последовательно работать с парами из списка аргументов.
+     * Например сначала он возьмет первые два массива (нулевой и первый) и прогонит через
+     * эту пару весь диапазон значений fraction (от 0 до 1). Затем возьмет ещё два массива
+     * (первый и второй) и снова прогонит fraction от 0 до 1 и так далее.
+     */
+    private fun animateTextColors() {
+
+        val paint: TextPaint = textViewColored.paint
+        val textWidth = paint.measureText(textViewColored.text.toString())
+
+        ValueAnimator.ofObject(
+            GradientArgbEvaluator(),
+            intArrayOf(Color.RED, Color.RED, Color.RED),
+            intArrayOf(Color.RED, Color.RED, Color.GREEN),
+            intArrayOf(Color.RED, Color.GREEN, Color.BLACK),
+            intArrayOf(Color.GREEN, Color.BLACK, Color.MAGENTA),
+            intArrayOf(Color.BLACK, Color.MAGENTA, Color.BLUE),
+            intArrayOf(Color.MAGENTA, Color.BLUE, Color.BLUE),
+            intArrayOf(Color.BLUE, Color.BLUE, Color.BLUE)
+        ).apply {
+            repeatMode = REVERSE
+            repeatCount = INFINITE
+            duration = 5000
+
+            interpolator = AccelerateDecelerateInterpolator()
+
+            addUpdateListener { animator ->
+
+                val textShader: Shader = LinearGradient(
+                    0f, 0f, textWidth, textViewColored.textSize,
+                    animator.animatedValue as IntArray, null,
+                    Shader.TileMode.CLAMP
+                )
+                textViewColored.paint.shader = textShader
+                textViewColored.invalidate()
+            }
+        }.start()
     }
 
     /**
@@ -73,7 +127,7 @@ class CustomBottomAppBarActivity : AppCompatActivity(), View.OnClickListener {
             when (value) {
                 hidden -> {
                     isBarVisible = false
-                    postDelayed({performHide()}, 100)
+                    postDelayed({ performHide() }, 100)
                 }
                 visible -> {
                     isBarVisible = true
