304f6d0ed28c6909ccfaaaceb431abe3d2f95152
ciscoff <ciscoff@ya.ru>
2021-02-18 10:38:25 +0300

    +

--- app/src/main/java/s/yarlykov/izisandbox/rx/test_rx/HeyFlowable.kt
+++ app/src/main/java/s/yarlykov/izisandbox/rx/test_rx/HeyFlowable.kt
@@ -5,20 +5,31 @@ import io.reactivex.schedulers.Schedulers
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
+/**
+ * Демонстрация работы backpressure. Оператор range (он же FlowableRange) генерит
+ * некоторое количество элементов, заполняя свой буфер (похоже, что его размер 128 элементов).
+ * Затем подписчик начинает забирать оттуда по одному элементу через каждые 5 ms и когда в буфере
+ * генератора остается ~32 элемента, то он снова заполняет его и генерит дополнительные 96
+ * элементов. Ну и т.д.
+ */
+
+var startTime = 0L
+
 class CustomSubscriber : Subscriber<Int> {
 
     var subscription : Subscription? = null
 
     override fun onSubscribe(s: Subscription?) {
+        startTime = System.currentTimeMillis()
         subscription = s
-        println("onSubscribe: subscription ${subscription != null}")
+        println("${System.currentTimeMillis() - startTime}: onSubscribe")
         s?.request(1)
     }
 
     override fun onNext(t: Int?) {
-        println("onNext $t")
+        println("${System.currentTimeMillis() - startTime}: onNext $t")
         try {
-            Thread.sleep(100L)
+            Thread.sleep(5L)
         } catch (e : Exception) {
             println("Exception")
         }
@@ -26,11 +37,11 @@ class CustomSubscriber : Subscriber<Int> {
     }
 
     override fun onError(t: Throwable?) {
-        println("onError")
+        println("${System.currentTimeMillis() - startTime}: onError")
     }
 
     override fun onComplete() {
-        println("onComplete")
+        println("${System.currentTimeMillis() - startTime}: onComplete")
     }
 }
 
@@ -41,16 +52,19 @@ class CustomSubscriber : Subscriber<Int> {
  * StrictSubscriber.request(..), а не метод источника !!!
  */
 fun main(args: Array<String>) {
+
     Flowable
-        .range(1, 10)
+        .range(1, 1000)
+        .doOnNext {
+            println("${System.currentTimeMillis() - startTime}: FlowableRange post item $it")
+        }
         .subscribeOn(Schedulers.newThread())
         .observeOn(Schedulers.io())
         .subscribe(CustomSubscriber())
 
     try {
         println("Thread '${Thread.currentThread().name}' is sleeping for 3 secs")
-        Thread.sleep(3000L)
+        Thread.sleep(10000L)
     } catch (e : Exception) {
-
     }
 }
