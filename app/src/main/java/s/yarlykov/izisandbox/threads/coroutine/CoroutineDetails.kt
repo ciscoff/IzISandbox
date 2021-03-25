a60b19c41379d9785e7d1fc5f761cacf91278b94
ciscoff <ciscoff@ya.ru>
2021-03-25 17:52:09 +0300

    +

--- app/src/main/java/s/yarlykov/izisandbox/threads/coroutine/CoroutineDetails.kt
+++ app/src/main/java/s/yarlykov/izisandbox/threads/coroutine/CoroutineDetails.kt
@@ -1,9 +1,10 @@
 package s.yarlykov.izisandbox.threads.coroutine
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
-import kotlin.coroutines.coroutineContext
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -40,66 +41,7 @@ suspend fun suspendFun3() {
     }
 }
 
-suspend fun testCoroutineScope(jobs : Array<Job?>) {
-    println("context inside testCoroutineScope=$coroutineContext")
-
-
-    try {
-
-        coroutineScope {
-
-            val job1 = launch {
-                delay(2000)
-                println("job1 is finished")
-            }
-
-            println("started job1")
-
-            val job2 = launch {
-                delay(2000)
-                println("job2 is finished")
-            }
-
-            println("started job2")
-
-            val job3 = launch {
-                delay(1000)
-                Integer.parseInt("a")
-                println("job3 is finished")
-            }
-
-            println("started job3")
-
-            jobs[0] = job1
-            jobs[1] = job2
-            jobs[2] = job3
-
-            println("context inside coroutineScope=${this.coroutineContext}")
-            println("job1 is active: ${jobs[0]?.isActive}, job2 is active: ${jobs[1]?.isActive}, job3 is active: ${jobs[2]?.isActive}")
-        }
-    } catch (e: Exception) {
-        println("EXCEPTION !!!")
-        println("job1 is active: ${jobs[0]?.isActive}, job2 is active: ${jobs[1]?.isActive}, job3 is active: ${jobs[2]?.isActive}")
-    }
-
-//    delay(100)
-//    println("job1 is active: ${jobs[0].isActive}, job2 is active: ${jobs[1].isActive}, job3 is active: ${jobs[2].isActive}")
-
-}
-
 fun main() {
-
-    val scope = CoroutineScope(Job() + Dispatchers.IO)
-
-    val jobs = Array<Job?>(3){null}
-
-    scope.launch(Dispatchers.IO) {
-        println("context inside launch=${coroutineContext}")
-        testCoroutineScope(jobs)
-    }
-
-    Thread.sleep(5000)
-
 
     /**
      * Чтобы не ставить TimeUnit.SECONDS.sleep(10) в main-потоке используем runBlocking
--- app/src/main/java/s/yarlykov/izisandbox/threads/coroutine/SupervisorCoroutineTest.kt
+++ app/src/main/java/s/yarlykov/izisandbox/threads/coroutine/SupervisorCoroutineTest.kt
@@ -1,4 +1,70 @@
 package s.yarlykov.izisandbox.threads.coroutine
 
-class SupervisorCoroutineTest {
+import kotlinx.coroutines.*
+import kotlin.coroutines.coroutineContext
+
+/**
+ * Здесь создаем вложенные корутины, их job'ы помещаем в массив, а потом достаем оттуда
+ * и смотрим статус в разых ситуациях.
+ *
+ * При исключении в job3 только его корутина станет не Active.
+ */
+suspend fun testSupervisorCoroutine(jobs: Array<Job?>) {
+    println("context inside ${object {}.javaClass.enclosingMethod?.name}: $coroutineContext")
+
+    val handler = CoroutineExceptionHandler { _, _ ->
+        println("Handle exception inside CoroutineExceptionHandler")
+        println("job1 is active: ${jobs[0]?.isActive}, job2 is active: ${jobs[1]?.isActive}, job3 is active: ${jobs[2]?.isActive}")
+    }
+
+    try {
+
+        supervisorScope {
+            println("context inside supervisorScope '${object {}.javaClass.enclosingMethod?.name}':${this.coroutineContext}")
+
+            val job1 = launch {
+                delay(DELAY_LONG)
+                println("job1 is finished")
+            }
+            println("job1 is started")
+
+            val job2 = launch {
+                delay(DELAY_LONG)
+                println("job2 is finished")
+            }
+            println("job2 is started")
+
+            val job3 = launch(handler) {
+                delay(DELAY_SHORT)
+                Integer.parseInt("exception")
+                println("job3 is finished")
+            }
+            println("job3 is started")
+
+            jobs[0] = job1
+            jobs[1] = job2
+            jobs[2] = job3
+            println("job1 is active: ${jobs[0]?.isActive}, job2 is active: ${jobs[1]?.isActive}, job3 is active: ${jobs[2]?.isActive}")
+        }
+    } catch (e: Exception) {
+    }
+}
+
+fun main() {
+
+    val scope = CoroutineScope(Job() + Dispatchers.IO)
+
+    val jobs = Array<Job?>(3) { null }
+
+    scope.launch(Dispatchers.IO) {
+        println("context inside launch '${object {}.javaClass.enclosingMethod?.name}': $coroutineContext")
+        testSupervisorCoroutine(jobs)
+        println("supervisorScope is finished")
+
+        withContext(Dispatchers.Main) {
+
+        }
+    }
+
+    Thread.sleep(5000)
 }
