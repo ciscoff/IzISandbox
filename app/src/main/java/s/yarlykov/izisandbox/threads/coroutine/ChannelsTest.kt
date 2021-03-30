44459d2be00605144a37d5a9eb08ae1ba642f2be
ciscoff <ciscoff@ya.ru>
2021-03-30 08:35:29 +0300

    +

--- app/src/main/java/s/yarlykov/izisandbox/threads/coroutine/ChannelsTest.kt
+++ app/src/main/java/s/yarlykov/izisandbox/threads/coroutine/ChannelsTest.kt
@@ -1,11 +1,65 @@
 package s.yarlykov.izisandbox.threads.coroutine
 
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
 import java.util.concurrent.Executors
 import kotlin.coroutines.suspendCoroutine
 
+@ExperimentalCoroutinesApi
 suspend fun testBuilder(scope: CoroutineScope) {
+
+    Observable.create<Int> { emitter ->
+        (1..10).forEach(emitter::onNext)
+    }
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+
+    flow<Int> {
+        coroutineScope {
+            val channel = produce<Int> {
+                launch {
+                    delay(1000)
+                    send(1)
+                }
+                launch {
+                    delay(1000)
+                    send(2)
+                }
+                launch {
+                    delay(1000)
+                    send(3)
+                }
+            }
+            channel.consumeEach {
+                // ...
+            }
+        }
+    }
+
+    val flow = channelFlow {
+        launch {
+            delay(1000)
+            send(1)
+        }
+        launch {
+            delay(1000)
+            send(2)
+        }
+        launch {
+            delay(1000)
+            send(3)
+        }
+    }
+
+
+
 
 //    scope.launch {
 //        println("scope=$scope")
@@ -33,7 +87,6 @@ suspend fun testBuilder(scope: CoroutineScope) {
  */
 
 fun main() {
-
 
 
     /**
