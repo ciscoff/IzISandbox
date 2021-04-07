30a4b28a03f39efc89cd6248857648ee8f5abcfa
ciscoff <ciscoff@ya.ru>
2021-04-07 07:17:06 +0300

    +

--- app/src/main/java/s/yarlykov/izisandbox/generics/Animal.kt
+++ app/src/main/java/s/yarlykov/izisandbox/generics/Animal.kt
@@ -1,4 +1,13 @@
 package s.yarlykov.izisandbox.generics
 
-class Animal {
-}
+open class Animal(val name: String)
+
+open class Cat(name: String) : Animal(name)
+class BlackCat(name: String) : Cat(name)
+class WhiteCat(name: String) : Cat(name)
+
+
+open class Dog(name: String) : Animal(name)
+class KindDog(name: String) : Dog(name)
+class AngryDog(name: String) : Dog(name)
+
--- app/src/main/java/s/yarlykov/izisandbox/generics/Main.kt
+++ app/src/main/java/s/yarlykov/izisandbox/generics/Main.kt
@@ -1,7 +1,76 @@
 package s.yarlykov.izisandbox.generics
 
+/**
+ * Базовый Generic - это просто болванка, заготовка.
+ * Он НЕ является ТИПОМ. Он является КЛАССОМ.
+ *
+ * T - это 'type parameter'
+ *
+ * parameter - это 'классификатор'
+ * argument - конкрентное значение
+ */
+interface Group<T> {
+    fun insert(t: T): Unit
+    fun fetch(): T
+}
+
+/**
+ * Для того, чтобы превратить класс в ТИП нужно вместо 'type parameter' указать 'type argument',
+ * то есть конкретный тип вместо классификатора. Один из способов это сделать - type projection:
+ *
+ * NOTE: 'in Dog' - это уже КОНКРЕТНЫЙ ТИП, благодара которому абстракстная болванка
+ * становится вот такого конкретного вида (это вытоматически сгенеренный код)
+ */
+
+// TODO Пример 1.
+class GroupDogIn : Group<in Dog> {
+    override fun insert(t: Dog) {}
+
+    override fun fetch(): Any? {
+        return null
+    }
+}
+
+// TODO Пример 2.
+class GroupDogOut : Group<out Dog> {
+    override fun insert(t: Nothing) {
+    }
+
+    override fun fetch(): Dog {
+        return Dog("nora")
+    }
+}
+
+/**
+ * И хотя Kotlin запрещает использовать type projection в качестве аргумента супертипа
+ * при декларации производных классов (пример выше), но не запрещает type projection для
+ * аургументов функций:
+ *
+ * NOTE: То есть две функции работают фактически с РАЗНЫМИ ТИПАМИ, хотя вроде бы у обеих
+ * аргументом является Group<Dog>. Но
+ */
+
+val myDogs = object : Group<Dog> {
+    override fun insert(t: Dog) {
+
+    }
+    override fun fetch(): Dog {
+        return Dog("Hunter")
+    }
+}
+
+
+// TODO Здесь компилятор подставил тип из примера 1.
+fun insertDogs(dogs: Group<in Dog>) {
+    dogs.insert(Dog("nora"))
+}
+
+// TODO Здесь компилятор подставил тип из примера 2.
+fun fetchDog(group : Group<out Dog>) : Dog {
+    return group.fetch()
+}
+
 fun main() {
-
-
-
+    insertDogs(myDogs)
+    fetchDog(myDogs)
 }
