2086b58b18a3e2a416c8bd7033eb74d915883759
Sergey Yarlykov <siyarlykov@mts.ru>
2022-02-06 15:43:54 +0300

    +

---
+++ app/src/main/java/s/yarlykov/izisandbox/ui/morph/MorphActivity.kt
@@ -0,0 +1,11 @@
+package s.yarlykov.izisandbox.ui.morph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MorphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_morph)
+    }
+}
